8321c0fe4e20b62696fe7923a0e7c91f32d70c95
Andrey Solovyh <andrejsolovyh6@gmail.com>
2026-03-19 23:48:32 +0300

    VKEdu: Добавил AppDetailsViewModel и редактировал под него AppDetailsScreen

--- app/src/main/java/dev/andre/vkeducation/presentation/presentation/appdetails/AppDetailsScreen.kt
+++ app/src/main/java/dev/andre/vkeducation/presentation/presentation/appdetails/AppDetailsScreen.kt
@@ -1,6 +1,7 @@
 package dev.andre.vkeducation.presentation.presentation.appdetails
 
 import android.widget.Toast
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
@@ -8,30 +9,63 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import dev.andre.vkeducation.R
 import dev.andre.vkeducation.presentation.AppRepository
 import dev.andre.vkeducation.presentation.presentation.theme.VkEducationTheme
 
 @Composable
-fun AppDetailsScreen(
-    appName: String, modifier: Modifier = Modifier, onBackClick: () -> Unit
-) {
-    val app = remember(appName) {
-        AppRepository.getAppByName(appName)
+fun AppDetailsRoute(
+    appName: String,
+    modifier: Modifier = Modifier,
+    onBackClick: () -> Unit,
+    viewModel: AppDetailsViewModel = viewModel()
+){
+    val state by viewModel.state.collectAsStateWithLifecycle()
+
+    LaunchedEffect(appName) {
+        viewModel.loadAppDetails(appName)
     }
 
+    when(val currentState = state) {
+        is AppDetailsState.Content ->
+            AppDetailsScreen(
+                appDetails = currentState.app,
+                modifier = modifier,
+                onBackClick = onBackClick,
+            )
+        is AppDetailsState.Error ->
+            ErrorContent()
+        is AppDetailsState.Loading ->
+            LoadingContent()
+    }
+}
+
+
+@Composable
+fun AppDetailsScreen(
+    modifier: Modifier = Modifier,
+    onBackClick: () -> Unit,
+    appDetails: App?
+) {
     val context = LocalContext.current
     val underDevelopmentText = stringResource(R.string.under_developement)
 
@@ -111,14 +145,24 @@ fun AppDetailsScreen(
 }
 
 
+
 @Preview
 @Composable
 private fun Preview() {
     VkEducationTheme {
         AppDetailsScreen(
-            appName = "Гильдия Героев: Экшен ММО РПГ",
             modifier = Modifier.fillMaxSize(),
             onBackClick = {},
+            appDetails = App(
+                name = "VK",
+                developer = "VK",
+                category = Category.APP,
+                ageRating = 12,
+                size = 95.3f,
+                screenshotUrlList = emptyList(),
+                iconUrl = "",
+                description = "Социальная сеть"
+            )
         )
     }
 }
